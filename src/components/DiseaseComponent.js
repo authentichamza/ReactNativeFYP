4dfbd759cdf327b41e5cf77dfdfe205c34d6ecd5
hamza4465989 <hamzacool555@gmail.com>
2021-02-27 01:07:59 +0500

    prototype almost ended

--- src/components/DiseaseComponent.js
+++ src/components/DiseaseComponent.js
@@ -15,76 +15,80 @@ import { color } from "react-native-reanimated";
 const db= firebase.firestore();
 let diseaseData =db.collection('datasetDisease')// storing connection of datasetDisease into diseaseData var
 let symptoms=[];
-diseaseData.get().then(querySnapshot => {
-  querySnapshot.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    // console.log(doc.data().Symptom);
-});
-});
+// diseaseData.get().then(querySnapshot => {
+//   querySnapshot.forEach((doc) => {
+//     doc.data() //is never undefined for query doc snapshots
+//     console.log(doc.data().Symptom);
+// });
+// });
 export default class App extends React.Component  {
   constructor(props) {
     super(props)
     this.state = {
       modalVisible: this.props.visible,// Modal visible variable received from page 3
-      disease:[],
-      symptoms:[
-        "Headache",
-        "cold",
-        "bleeding",
-        "fever",
-      ],
-      item:',',
-      sessionSymptom:this.props.Symptom,
+      result:[],
+      symptoms:[],
+      item:'______',
+      dis:'',
+      sessionSymptom:[],
     }
   }
-  componentWillReceiveProps({Disease}) {
-    this.setState({...this.state,someProp})
-  }
   changeDis(dis){
-    this.setState({disease:dis})
+    this.setState({result:dis})
+
+  }
+  setEnteredSymptoms(sym){
+    this.setState({symptoms:sym})
   }
   setModalVisible(visible){// changing visibility method 
     this.setState({ modalVisible: visible });
   }
+  change(dis){
+    this.setState({dis:dis});
+  }
+  
   render() {
-    
     return (
-      
-      <View style={styles.centeredView}>
-        {this.state.disease.map((dis)=>{
-          return(
+      <View>
             <View style={styles.centeredView}>
-          <Modal //Modal with disease Name and flat list of specific disease symptoms
-            animationType="slide"
-            transparent={true}
-            visible={this.state.modalVisible}
-            onRequestClose={() => {
-              console.log("Modal has been closed.");
-            }}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>{dis}</Text>
-                <FlatList
-                  data={this.state.symptoms}
-                  renderItem={({item}) => <Text style={styles.item}>{this.state.item}</Text>}
-                  numColumns={2} // 2 symptoms per row
-                />
-                <TouchableHighlight
-                  style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-                  onPress={() => {
-                    this.setModalVisible(!this.state.modalVisible);
-                  }}
-                >
-                  <Text style={styles.textStyle}>Hide Modal</Text>
-                </TouchableHighlight>
+            <Modal //Modal with disease Name and flat list of specific disease symptoms
+              animationType="slide"
+              transparent={true}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                console.log("Modal has been closed.");
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>{this.state.dis}</Text>
+                  <FlatList
+                    data={this.state.symptoms}
+                    renderItem={({item}) => <Text style={styles.item}>{this.state.item}</Text>}
+                    numColumns={2} // 2 symptoms per row
+                  />
+                  <TouchableHighlight
+                    style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+                    onPress={() => {
+                      this.setModalVisible(!this.state.modalVisible);
+                    }}
+                  >
+                    <Text style={styles.textStyle}>Hide Modal</Text>
+                  </TouchableHighlight>
+                </View>
               </View>
-            </View>
-          </Modal>
+            </Modal>
+          </View>
+          
+          {this.state.result.map((dis)=>{
+            let x=this.setEnteredSymptoms(this.state.symptoms,Object.values(dis))
+            return(
           <View style={styles.container}> 
-            <TouchableHighlight style={styles.back} onPress={()=>this.setModalVisible(true)}>  
+            <TouchableHighlight style={styles.back} 
+            onPress={()=>{this.setModalVisible(true);this.change(dis);this.fetchSymptoms(dis);
+            }}>  
                 <View style={styles.DisEntry}>
-                    <Text style={styles.text}>{dis}</Text>
+                    <Text style={styles.text}>{Object.keys(dis)}</Text>
                     <ProgressCircle
                         percent={30}
                         radius={50}
@@ -93,12 +97,12 @@ export default class App extends React.Component  {
                         shadowColor="#999"
                         bgColor="#fff"
                     >
-                    <Text style={{ fontSize: 18 }}>{'70%'}</Text>
+                    <Text style={{ fontSize: 18 }}>{x}</Text>
                   </ProgressCircle>
                 </View>
             </TouchableHighlight>
-          </View></View>)})}
-        
+          </View>
+          )})}
       </View>
     );
   }
